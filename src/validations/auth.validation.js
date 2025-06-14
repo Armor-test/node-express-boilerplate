1e9eb4c9bc351c390dede5c26e1a489d800c227d
m.osama <mohamed.osa.22@gmail.com>
2025-06-14 05:01:24 +0300

    Add password validation on register #302

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -4,7 +4,7 @@ const { password } = require('./custom.validation');
 const register = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
+    password: Joi.string().required().max(64).custom(password),
     name: Joi.string().required(),
   }),
 };
